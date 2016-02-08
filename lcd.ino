917311a6d8266e87df21af1d89f6df30a6acad72
Gabriel Capella <gabriel@capella.pro>
2016-02-08 13:12:46 -0200

    Create lcd.ino
    LCD example

---
+++ lcd.ino
@@ -0,0 +1,18 @@
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  //clean
+  lcd.clear();
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
